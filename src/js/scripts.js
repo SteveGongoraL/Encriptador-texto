c3bb6a8261175d7f7ff7e7e9dbc9926413e2f981
SteveGongoraL <stv.brayton.gongora@gmail.com>
2024-08-23 00:24:02 -0600

    Se agregaron las funciones para copiar en el portapapeles, mostrar resultado y ocultar elementos

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,30 +7,42 @@ const politicaCifrar = [
 ];
 
 
+// Funcion para el btn Encriptar
 function mostrarEncriptado(){
+    // Hacer el encriptado del texto y almacenarlo
     let resultadoEncriptado = encriptarTexto("txtAreaTexto");
-    console.log(resultadoEncriptado);
-    limpiarImput();
+    // Cambiar el texto del parrafo por el texto Encriptado
+    cambiarTextoParrafo("desencriptar-parrafo", resultadoEncriptado);
+    // Copiar al portapapeles
+    copiarAlPortapapeles(resultadoEncriptado);
+    // Limpiar el textArea y ocultar la imagen y titulo de la section de respuesta
+    clearAndHide();
     return;
 }
 
+// Funcion para el btn Desencriptar
 function mostrarDesencriptado(){
+    // Hacer el encriptado del texto y almacenarlo
     let resultadoDesencriptado = desencriptarTexto("txtAreaTexto");
-    console.log(resultadoDesencriptado);
-    limpiarImput();
+    // Cambiar el texto del parrafo por el texto Desencriptado
+    cambiarTextoParrafo("desencriptar-parrafo", resultadoDesencriptado);
+    // Copiar al portapapeles
+    copiarAlPortapapeles(resultadoDesencriptado);
+    // Limpiar el textArea y ocultar la imagen y titulo de la section de respuesta
+    clearAndHide();
     return;
 }
 
+// Encriptar el texto
 function encriptarTexto(idCampo){
     let textoEncriptar = document.getElementById(idCampo).value;
-    
     for(const [letra, valor] of politicaCifrar){
         textoEncriptar = textoEncriptar.split(letra).join(valor);
     }
-    
     return textoEncriptar;
 }
 
+// Desencriptar el texto
 function desencriptarTexto(idCampo){
     let textoDesencriptar = document.getElementById(idCampo).value;
     for(const [letra, valor] of politicaCifrar){
@@ -39,8 +51,38 @@ function desencriptarTexto(idCampo){
     return textoDesencriptar;
 }
 
+// Funcion para preparar el desencriptado
+function clearAndHide(){
+    limpiarImput();
+    ocultarElementos();
+}
+
+// Cambiar el texto
+function cambiarTextoParrafo(idElementoACambiar, textoACambiar){
+    let parrafoACambiar = document.getElementById(idElementoACambiar);
+    parrafoACambiar.innerHTML = textoACambiar;
+    return;
+}
+
 // Limpiar el textarea
 function limpiarImput(){
     document.getElementById("txtAreaTexto").value = "";
+    return;
+}
+
+// Ocultar elementos
+function ocultarElementos(){
+    document.getElementById("desencriptar-imagen").style.display = "none";
+    document.getElementById("desencriptar-titulo").style.display = "none";
+}
+
+// Copiar en el portapapeles
+function copiarAlPortapapeles(textoACopiar){
+    // Api clipboard
+    navigator.clipboard.writeText(textoACopiar).then(function(){
+        console.log("Texto copiado");
+    }).catch(function(error){
+        console.error("Error al copiar", error);
+    });
     return;
 }
